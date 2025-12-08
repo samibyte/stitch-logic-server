975b2bdae80ebf17f4eb8e8709202410df81f82e
samibyte <samibyte.web@gmail.com>
2025-12-08 14:31:22 +0600

    feat: implement mongoose setup

---
+++ config/db.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  const uri = process.env.DB_URI;
+
+  if (!uri) {
+    throw new Error("DB_URI is not defined in environment");
+  }
+
+  try {
+    const conn = await mongoose.connect(process.env.DB_URI, {
+      dbName: "stitchLogic",
+      autoIndex: false,
+    });
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
