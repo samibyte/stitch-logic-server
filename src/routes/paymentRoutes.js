3c1ceb57f3e106ef80845ed6856d3ad2eb0bb961
samibyte <samibyte.web@gmail.com>
2025-12-24 04:14:56 +0600

    fix: remove unused payment routes

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,14 +1,5 @@
 import express from "express";
-import {
-  createPayment,
-  getPaymentByOrder,
-} from "../controllers/paymentController.js";
-import { verifyFBToken } from "../middlewares/auth.js";
-import { checkRole } from "../middlewares/roleCheck.js";
 
 const router = express.Router();
-
-router.post("/", verifyFBToken, checkRole("buyer"), createPayment);
-router.get("/:orderId", verifyFBToken, getPaymentByOrder);
 
 export default router;
